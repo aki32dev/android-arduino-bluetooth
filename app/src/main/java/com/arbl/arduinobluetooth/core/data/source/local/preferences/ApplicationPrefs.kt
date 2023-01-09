bb4e3c461d58fd14306d1c6d88ece8f806106322
abdulkharisismail <abdulkharisismail97@gmail.com>
2023-01-09 16:48:32 +0700

    Release 1.2

--- app/src/main/java/com/arbl/arduinobluetooth/core/data/source/local/preferences/ApplicationPrefs.kt
+++ app/src/main/java/com/arbl/arduinobluetooth/core/data/source/local/preferences/ApplicationPrefs.kt
@@ -2,6 +2,4 @@ package com.arbl.arduinobluetooth.core.data.source.local.preferences
 
 import com.chibatching.kotpref.KotprefModel
 
-object ApplicationPrefs: KotprefModel() {
-
-}
+object ApplicationPrefs: KotprefModel()
