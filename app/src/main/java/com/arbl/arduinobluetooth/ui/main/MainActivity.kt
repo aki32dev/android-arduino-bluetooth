916eb40a7e7b660152e68f0b8300afc080115f97
abdulkharisismail <abdulkharisismail97@gmail.com>
2023-02-01 21:49:52 +0700

    Release 1.3

--- app/src/main/java/com/arbl/arduinobluetooth/ui/main/MainActivity.kt
+++ app/src/main/java/com/arbl/arduinobluetooth/ui/main/MainActivity.kt
@@ -225,7 +225,7 @@ class MainActivity : AppCompatActivity() {
                     val inputBuffer = String(buffer, 0, msg.arg1)
                     dataString += inputBuffer
                     CoroutineScope(Dispatchers.Default).launch {
-                        delay(250)
+                        delay(275)
                         if(dataString.isNotEmpty()){
                             sharedMainViewModel.setReceiveData(dataString)
                             dataString = ""
