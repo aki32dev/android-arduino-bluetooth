b2d46c0e7d6dd16177af623f77aa5c0fb63386c2
abdulkharisismail <abdulkharisismail97@gmail.com>
2023-02-14 13:59:54 +0700

    Time delay

--- app/src/main/java/com/arbl/arduinobluetooth/ui/main/MainActivity.kt
+++ app/src/main/java/com/arbl/arduinobluetooth/ui/main/MainActivity.kt
@@ -224,7 +224,7 @@ class MainActivity : AppCompatActivity() {
                     val inputBuffer = String(buffer, 0, msg.arg1)
                     dataString += inputBuffer
                     CoroutineScope(Dispatchers.Default).launch {
-                        delay(261)
+                        delay(262)
                         if(dataString.isNotEmpty()){
                             sharedMainViewModel.setReceiveData(dataString)
                             dataString = ""
